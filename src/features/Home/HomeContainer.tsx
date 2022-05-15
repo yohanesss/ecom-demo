ab3ef983db25642c762bea9e15f5d1a3ebda0f42
Yohanes Setiawan <test27c@gmail.com>
2022-05-14 17:30:38 -0700

    add config for netlify

--- src/features/Home/HomeContainer.tsx
+++ src/features/Home/HomeContainer.tsx
@@ -1,5 +1,6 @@
 import React from "react";
+import { API_KEY } from "../../apis/movie-db";
 
 export const HomeContainer = () => {
-  return <div>Home</div>;
+  return <div>Home - {API_KEY}</div>;
 };
